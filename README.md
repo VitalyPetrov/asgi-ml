ef8c37315f2a650a02c4a1c29402f15852e38502
VitalyPetrov <VitaliyPetrov18@gmail.com>
2020-10-07 00:24:31 +0300

    create FastAPI app instance + design configuration management

---
+++ README.md
@@ -0,0 +1,10 @@
+## asgi-ml
+
+
+Pet-project to be used as a baseline for ML-based microservices development
+
+### Drives specification:
+
+<b>Web:</b> [FastAPI](https://github.com/tiangolo/fastapi) \
+<b>ML:</b> scikit-learn, implicit, CatBoost, any-not-heavy-model \
+<b>DB engine:</b> SQLAlchemy (with any compatible driver)
---
+++ src/conf.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from pydantic import validator, BaseSettings
+from dotenv import load_dotenv
+
+
+PROJECT_ROOT = Path(os.path.dirname(os.path.abspath(__file__)).parent)
+load_dotenv()
+
+
+class MLSettings(BaseSettings):
+    pass
+
+
+class Settings(BaseSettings):
+    api_prefix: str = os.getenv("APP_API_PREFIX")
+
+    @validator("api_prefix", pre=True)
+    def is_valid_api_prefix(cls, value: str) -> str:
+        if not isinstance(value, str):
+            raise ValueError(
+                f"API prefix should have str type, got {type(value)} instead"
+            )
+        return value
+
+
+settings = Settings()
